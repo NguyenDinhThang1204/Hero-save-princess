79531673b1d6204be53650bdbff782ad92de7ae0
TaiPhung02 <2051063445@e.tlu.edu.vn>
2023-12-26 00:30:26 +0700

    Create Damage text instance on UnityAction

---
+++ Source/Assets/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject damageTextPrefab;
+    public GameObject healthTextPrefab;
+
+    public Canvas gameCanvas;
+
+    private void Awake()
+    {
+        gameCanvas = GetComponent<Canvas>();
+
+    }
+
+    private void OnEnable()
+    {
+        CharacterEvents.characterDamaged.AddListener(CharacterTookDamage);
+        CharacterEvents.characterHealed.AddListener(CharacterHealed);
+    }
+
+    private void OnDisable()
+    {
+        CharacterEvents.characterDamaged.RemoveListener(CharacterTookDamage);
+        CharacterEvents.characterHealed.RemoveListener(CharacterHealed);
+    }
+    public void CharacterTookDamage(GameObject character, int damageReceived)
+    {
+        Vector3 spawnPosition = Camera.main.WorldToScreenPoint(character.transform.position);
+
+        TMP_Text tmpText = Instantiate(damageTextPrefab, spawnPosition, Quaternion.identity, gameCanvas.transform)
+            .GetComponent<TMP_Text>();
+
+        tmpText.text = damageReceived.ToString();
+    }
+
+    public void CharacterHealed(GameObject character, int healthRestored)
+    {
+        Vector3 spawnPosition = Camera.main.WorldToScreenPoint(character.transform.position);
+
+        TMP_Text tmpText = Instantiate(healthTextPrefab, spawnPosition, Quaternion.identity, gameCanvas.transform)
+            .GetComponent<TMP_Text>();
+
+        tmpText.text = healthRestored.ToString();
+    }
+}
